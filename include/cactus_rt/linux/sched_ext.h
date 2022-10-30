2f9b0255d1c0a39746009d6d89280ce345508902
Stephanie <eng.steph@gmail.com>
2022-10-30 18:02:14 -0400

    Use sched_setattr

---
+++ include/cactus_rt/linux/sched_ext.h
@@ -0,0 +1,40 @@
+#ifndef CACTUS_RT_LINUX_SCHED_EXT_H_
+#define CACTUS_RT_LINUX_SCHED_EXT_H_
+
+#include <sys/syscall.h>
+#include <unistd.h>
+
+#include <cstdint>
+
+struct sched_attr {
+  uint32_t size;
+
+  uint32_t sched_policy;
+  uint64_t sched_flags;
+
+  /* SCHED_NORMAL, SCHED_BATCH */
+  int32_t sched_nice;
+
+  /* SCHED_FIFO, SCHED_RR */
+  uint32_t sched_priority;
+
+  /* SCHED_DEADLINE (nsec) */
+  uint64_t sched_runtime;
+  uint64_t sched_deadline;
+  uint64_t sched_period;
+};
+
+inline int sched_setattr(pid_t                    pid,
+                         const struct sched_attr *attr,
+                         unsigned int             flags) {
+  return syscall(SYS_sched_setattr, pid, attr, flags);
+}
+
+inline int sched_getattr(pid_t              pid,
+                         struct sched_attr *attr,
+                         unsigned int       size,
+                         unsigned int       flags) {
+  return syscall(SYS_sched_getattr, pid, attr, size, flags);
+}
+
+#endif
