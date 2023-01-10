3135217387e78af70cb11e9ceedb90def7d5a153
Shuhao Wu <shuhao@shuhaowu.com>
2023-01-09 21:46:38 -0500

    Build updates

--- docker/scripts/01-build.sh
+++ docker/scripts/01-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cmake -B${CACTUS_RT_BUILD_DIR} -DENABLE_CLANG_TIDY=ON
+cmake -B${CACTUS_RT_BUILD_DIR} -DENABLE_CLANG_TIDY=ON -DBUILD_DOCS=ON
 cmake --build ${CACTUS_RT_BUILD_DIR} -j $(nproc)
