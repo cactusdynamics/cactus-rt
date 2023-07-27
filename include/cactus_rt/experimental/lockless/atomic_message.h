20512b98afa7922bcd3d18b0921bb2757fdfdf23
Shuhao Wu <shuhao@shuhaowu.com>
2023-07-27 00:28:32 -0400

    Added atomic message experimental data structure

---
+++ include/cactus_rt/experimental/lockless/atomic_message.h
@@ -0,0 +1,65 @@
+#ifndef CACTUS_RT_EXPERIMENTAL_LOCKLESS_ATOMIC_MESSAGE_H_
+#define CACTUS_RT_EXPERIMENTAL_LOCKLESS_ATOMIC_MESSAGE_H_
+
+#include <atomic>
+#include <utility>
+
+namespace cactus_rt::experimental::lockless {
+
+/**
+ * @brief A simple wrapper around a std::atomic to provide a real-time safe
+ * compare-and-swap operation (via .Modify()).
+ *
+ * @tparam T the data type to be wrapped. std::atomic<T>::is_always_lock_free must be true.
+ */
+template <typename T, std::memory_order memory_order = std::memory_order_seq_cst>
+class AtomicMessage {
+  static_assert(std::atomic<T>::is_always_lock_free, "AtomicMessage<T> requires type T to be usable with std::atomic<T>::is_always_lock_free");
+  std::atomic<T> data_;
+
+ public:
+  /**
+   * @brief Constructs a new AtomicMessage using the constructor of the
+   * underlying type.
+   */
+  template <typename... Args>
+  AtomicMessage(Args&&... args) : data_(std::forward<Args>(args)...) {}
+
+  /**
+   * @brief Returns a copy of the data in a lock-less manner.
+   */
+  T Read() const noexcept {
+    return data_.load(memory_order);
+  }
+
+  /**
+   * @brief Writes into the variable in a lock-less manner.
+   */
+  void Write(const T& data) noexcept {
+    return data_.store(data, memory_order);
+  }
+
+  /**
+   * @brief Modify the variable via an operator function in a lock-less manner (but not wait-free).
+   *
+   * @param f A function that takes the old value and returns the new value.
+   * Maybe called multiple times. To be real-time safe, this can be a function
+   * pointer or a lambda function that doesn't capture anything that can cause
+   * dynamic memory allocation.
+   */
+  template <typename Func>
+  void Modify(Func f) noexcept {
+    auto old_value = Read();
+
+    while (true) {
+      T new_value = f(old_value);
+      if (data_.compare_exchange_weak(old_value, new_value, memory_order)) {
+        break;
+      }
+    }
+  }
+};
+
+}  // namespace cactus_rt::experimental::lockless
+
+#endif
