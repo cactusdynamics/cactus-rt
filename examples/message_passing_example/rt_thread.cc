71156eb297aed0a8a8a3c57394ca127133df74c5
Jelle Hierck <j.j.hierck@student.utwente.nl>
2024-10-10 17:39:55 +0200

    Added missing include and math functions

--- examples/message_passing_example/rt_thread.cc
+++ examples/message_passing_example/rt_thread.cc
@@ -1,12 +1,15 @@
 #include "rt_thread.h"
 
+#define _USE_MATH_DEFINES
+#include "cmath"
+
 cactus_rt::CyclicThread::LoopControl RtThread::Loop(int64_t ellapsed_ns) noexcept {
   const double ellapsed_ms = static_cast<double>(ellapsed_ns) / 1'000'000.0;
 
   const double period = 1000.0;
   const double amplitude = 1.0;
 
-  data_logger_->EmplaceData(ellapsed_ms, amplitude * cos(2 * M_PI / period * ellapsed_ms));
+  data_logger_->EmplaceData(ellapsed_ms, amplitude * std::cos(2 * M_PI / period * ellapsed_ms));
 
   ++iterations_;
   return iterations_ >= max_iterations_ ? LoopControl::Stop : LoopControl::Continue;
