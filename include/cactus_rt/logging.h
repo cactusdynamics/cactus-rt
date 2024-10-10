3d7fe7af6cbec10210345deb9b7b5941bff89b43
Jelle Hierck <j.j.hierck@student.utwente.nl>
2024-10-10 17:39:34 +0200

    Moved implementation to logging.cc file

--- include/cactus_rt/logging.h
+++ include/cactus_rt/logging.h
@@ -3,11 +3,13 @@
 
 #include <cstdint>
 #include <memory>
+#include <string>
 
 #include "quill/Frontend.h"
 #include "quill/Logger.h"
 #include "quill/backend/BackendOptions.h"
-#include "quill/sinks/ConsoleSink.h"
+#include "quill/core/Common.h"
+#include "quill/core/PatternFormatterOptions.h"
 #include "quill/sinks/Sink.h"
 
 namespace cactus_rt::logging {
@@ -60,17 +62,7 @@ using Logger = BoundedDroppingLogger;
  *
  * @return quill::BackendOptions
  */
-static quill::BackendOptions DefaultBackendOptions() {
-  // Many of the default options set by Quill are already OK for real-time logging
-  quill::BackendOptions default_backend_options;
-
-  // Disable strict timestamp order by setting the grace period to 0 - this will be faster, but logs may appear out of order
-  // See quill::BackendOptions documentation for more info
-  // TODO: Setting this to 0 causes assertion error in Debug builds. Perhaps a bug in Quill (https://github.com/odygrd/quill/issues/605)?
-  default_backend_options.log_timestamp_ordering_grace_period = std::chrono::milliseconds(1);
-
-  return default_backend_options;
-}
+quill::BackendOptions DefaultBackendOptions();
 
 /**
  * Create a ConsoleSink object with console colours.
@@ -79,24 +71,12 @@ static quill::BackendOptions DefaultBackendOptions() {
  * @return A pointer to the created sink. If a sink with that ID already
  * existed, a pointer to the existing sink is returned instead.
  */
-static std::shared_ptr<quill::Sink> DefaultConsoleSink(std::string sink_name) {
-  // TODO: (QUILL v7.3.0): move implementation to .cpp file
-  return quill::Frontend::create_or_get_sink<quill::ConsoleSink>(
-    sink_name,
-    true  // Enable console colours
-  );
-}
+std::shared_ptr<quill::Sink> DefaultConsoleSink(std::string sink_name);
 
 /**
  * Get a default pattern format.
  */
-static quill::PatternFormatterOptions DefaultPatternFormatterOptions() {
-  // TODO: (QUILL v7.3.0): move implementation to .cpp file
-  return quill::PatternFormatterOptions(
-    "[%(time)][%(log_level_short_code)][%(logger)][%(file_name):%(line_number)] %(message)",
-    "%Y-%m-%d %H:%M:%S.%Qns"
-  );
-}
+quill::PatternFormatterOptions DefaultPatternFormatterOptions();
 
 /**
  * Create a Logger object with default settings which will write logs to the
@@ -112,14 +92,8 @@ static quill::PatternFormatterOptions DefaultPatternFormatterOptions() {
  * @return A pointer to the created logger. If a logger with that ID already
  * existed, a pointer to the existing logger is returned instead.
  */
-static cactus_rt::logging::Logger* DefaultLogger(std::string logger_name) {
-  // TODO: (QUILL v7.3.0): move implementation to .cpp file
-  return cactus_rt::logging::Frontend::create_or_get_logger(
-    logger_name,
-    DefaultConsoleSink(logger_name + "_ConsoleSink"),
-    DefaultPatternFormatterOptions()
-  );
-}
+cactus_rt::logging::Logger* DefaultLogger(std::string logger_name);
+
 }  // namespace cactus_rt::logging
 
 #endif
---
+++ src/cactus_rt/logging.cc
@@ -0,0 +1,48 @@
+#include "cactus_rt/logging.h"
+
+#include <chrono>
+#include <memory>
+#include <string>
+
+#include "quill/Logger.h"
+#include "quill/backend/BackendOptions.h"
+#include "quill/core/PatternFormatterOptions.h"
+#include "quill/sinks/ConsoleSink.h"
+#include "quill/sinks/Sink.h"
+
+quill::BackendOptions cactus_rt::logging::DefaultBackendOptions() {
+  // Many of the default options set by Quill are already OK for real-time logging
+  quill::BackendOptions default_backend_options;
+
+  // Disable strict timestamp order by setting the grace period to 0 - this will be faster, but logs may appear out of order
+  // See quill::BackendOptions documentation for more info
+  // TODO: Setting this to 0 causes assertion error in Debug builds. Perhaps a bug in Quill (https://github.com/odygrd/quill/issues/605)?
+  default_backend_options.log_timestamp_ordering_grace_period = std::chrono::milliseconds(1);
+
+  return default_backend_options;
+}
+
+cactus_rt::logging::Logger* cactus_rt::logging::DefaultLogger(std::string logger_name) {
+  // TODO: (QUILL v7.3.0): move implementation to .cpp file
+  return cactus_rt::logging::Frontend::create_or_get_logger(
+    logger_name,
+    DefaultConsoleSink(logger_name + "_ConsoleSink"),
+    DefaultPatternFormatterOptions()
+  );
+}
+
+std::shared_ptr<quill::Sink> cactus_rt::logging::DefaultConsoleSink(std::string sink_name) {
+  // TODO: (QUILL v7.3.0): move implementation to .cpp file
+  return cactus_rt::logging::Frontend::create_or_get_sink<quill::ConsoleSink>(
+    sink_name,
+    true  // Enable console colours
+  );
+}
+
+quill::PatternFormatterOptions cactus_rt::logging::DefaultPatternFormatterOptions() {
+  // TODO: (QUILL v7.3.0): move implementation to .cpp file
+  return quill::PatternFormatterOptions(
+    "[%(time)][%(log_level_short_code)][%(logger)][%(file_name):%(line_number)] %(message)",
+    "%Y-%m-%d %H:%M:%S.%Qns"
+  );
+}
